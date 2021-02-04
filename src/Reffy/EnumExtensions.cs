62c539665e88266e79b87d6e59a180cffcee8f58
midoliy <midorikawa@tech-leaves.com>
2021-02-04 21:55:32 +0900

    💡throwの書き漏らしを修正

--- src/Reffy/EnumExtensions.cs
+++ src/Reffy/EnumExtensions.cs
@@ -11,7 +11,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, byte value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -20,7 +20,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, sbyte value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -29,7 +29,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, ushort value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -38,7 +38,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, short value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -47,7 +47,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, uint value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -56,7 +56,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, int value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -65,7 +65,7 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, ulong value)
-            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum ? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
 
         /// <summary>
         /// 整数値からEnum型の値を取得する
@@ -74,6 +74,6 @@ namespace Reffy
         /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
         /// <returns>Enum型インスタンス</returns>
         public static object GetEnumValue(this Type type, long value)
-            => type.IsEnum? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+            => type.IsEnum? Enum.ToObject(type, value) : throw new Exception("'type' is not Enum type.");
     }
 }
