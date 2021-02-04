8ed596266265f4f7e53c35d66463f282094bc6f6
midoliy <midorikawa@tech-leaves.com>
2021-02-04 21:35:28 +0900

    💡Typeからデフォルトインスタンスを生成する拡張メソッドを追加

---
+++ src/Reffy/EnumExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Reffy
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, byte value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, sbyte value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, ushort value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, short value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, uint value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, int value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, ulong value)
+            => type.IsEnum ? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+
+        /// <summary>
+        /// 整数値からEnum型の値を取得する
+        /// </summary>
+        /// <param name="type">Enum型のTypeインスタンス</param>
+        /// <param name="value">Enum型内で定義されている値に割り当てられている整数値</param>
+        /// <returns>Enum型インスタンス</returns>
+        public static object GetEnumValue(this Type type, long value)
+            => type.IsEnum? Enum.ToObject(type, value) : new Exception("'type' is not Enum type.");
+    }
+}
---
+++ src/Reffy/TypeExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Reflection;
+
+namespace Reffy
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object CreateDefaultInstance(this Type type)
+        {
+            // 判別共用体(F#)
+            if (FSharpType.IsUnion(type, FSharpOption<BindingFlags>.None))
+            {
+                var ctor = FSharpType.GetUnionCases(type, FSharpOption<BindingFlags>.None).FirstOrDefault();
+                if (ctor == null)
+                    throw new Exception("Invalid type.");
+                var ps = ctor.GetFields();
+                var qs = new object[ps.Length];
+                for (var i = 0; i < ps.Length; i++)
+                {
+                    qs[i] = ps[i].PropertyType.IsValueType
+                        ? Activator.CreateInstance(ps[i].PropertyType)
+                        : null;
+                }
+
+                return FSharpValue.MakeUnion(ctor, qs, FSharpOption<BindingFlags>.None);
+            }
+
+            // 列挙型
+            if (type.IsEnum)
+                return Enum.GetValues(type).GetValue(0);
+
+            // 構造体
+            if (type.IsValueType)
+                return Activator.CreateInstance(type);
+
+            // クラス
+            if (type.IsClass)
+            {
+                var ctor = type.GetConstructors().FirstOrDefault();
+                if (ctor is null)
+                    throw new Exception("Public constructor does not exist.");
+
+                var ps = ctor.GetParameters();
+                var qs = new object[ps.Length];
+                for (int i = 0; i < ps.Length; i++)
+                {
+                    qs[i] = ps[i].ParameterType.IsValueType
+                        ? Activator.CreateInstance(ps[i].ParameterType)
+                        : null;
+                }
+                return Activator.CreateInstance(type, qs);
+            }
+
+            throw new ArgumentException("'type' is not supported.");
+        }
+    }
+}
