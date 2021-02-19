e2ce35719a625f5fcde109fbed200e4ed3250ab6
midoliy <midorikawa@tech-leaves.com>
2021-02-19 15:27:51 +0900

    ✨コンストラクタ生成用の拡張メソッド追加

---
+++ src/Net40Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Reffy.Expressions;
+
+namespace Net40Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/Net50Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Reffy.Expressions;
+
+namespace Net50Console
+{
+    public class Foo
+    {
+        public int MyProperty { get; set; }
+        public double MyProperty2 { get; set; }
+
+        public Foo()
+        {
+
+        }
+
+        public Foo(int i, double d)
+        {
+            MyProperty = i;
+            MyProperty2 = d;
+        }
+    }
+
+    public class Test
+    {
+        private const int length = 1_000;
+
+        [Benchmark]
+        public void UseActivator()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                var foo = Activator.CreateInstance(typeof(Foo), new object[] { i, (double)i });
+            }
+        }
+
+        [Benchmark]
+        public void UseConstructorNoCache()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                var foo = typeof(Foo).ConstructorNoCache(i, (double)i);
+            }
+        }
+
+        [Benchmark]
+        public void UseConstructor()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                var foo = typeof(Foo).Constructor(i, (double)i);
+            }
+        }
+
+        [Benchmark]
+        public void UseRestrictedConstructor()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                var foo = typeof(Foo).RestrictedConstructor(i, (double)i);
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Test>();
+        }
+    }
+}
